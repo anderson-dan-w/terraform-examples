6236038fdd06a6343a04f4ab4014af5ab9f8e4b5
Dan W Anderson <dan@sigopt.com>
2019-06-14 16:49:40 -0700

    starting basic tf example

---
+++ src/main.tf
@@ -0,0 +1,33 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "dan-tf-http-sg"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "dan-tf" {
+  ami = "ami-005bdb005fb00e791"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags = {
+    Name = "dan-tf-example"
+  }
+}
